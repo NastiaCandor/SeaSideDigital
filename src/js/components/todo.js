f42f7fbef99d56e76a53e130b057b059a68386ed
Anastasia <andubova98@gmail.com>
2024-02-16 00:07:07 +0300

    feat: add task filters

--- src/js/components/todo.js
+++ src/js/components/todo.js
@@ -4,118 +4,153 @@ export default class Todo {
   constructor() {
     this.form = document.querySelector('.todo__form');
     this.list = document.querySelector('.todo__list');
+    this.filters = document.querySelectorAll('.todo__filter-input');
     this.storage = this.restoreFromLocalStorage();
   }
 
+  // Инициализация
   init() {
     this.activateForm();
     this.activateList();
+    this.activateFilter();
   }
 
-  // Task adding functionality
+  // Функционал добавления новой задачи
   activateForm() {
     this.form.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      // collecting task info
+      // сбор данных новой задачи
       const formData = new FormData(this.form);
       const task = {};
       formData.forEach(function(value, key){
         task[key] = value;
       });
 
+      // Подготовка состояния задачи
       task.state = 'active';
       task.id = this.storage.length > 0 ? this.storage[this.storage.length - 1].id + 1 : 0;
 
-      // addint it to the list
-      this.addTask(task);
-      // add task to local storage
+      // добавление задачи в список
+      this.addTaskToDOM(task);
+      // добавление задачи в local storage
       this.addTaskToLocalStorrage(task);
-      // clear form inputs
+      // очистка формы новой задачи
       this.form.reset();
     });
   }
 
+  // Функционал фильтрации списка задач
+  activateFilter() {
+    this.filters.forEach((filter) => {
+      filter.addEventListener('click', () => {
+        // очистка списка в DOM
+        this.clearList();
+        // если все задачи, то вывести все задачи в DOM
+        if (filter.value === 'all') {
+          this.fillList(this.storage);
+        } else {
+          // если фильт, то составляется новый массив согласно фильтру, затем вывод задачи в DOM
+          const newList = this.storage.filter(task => task.state === filter.value);
+          this.fillList(newList);
+        }
+      })
+    });
+  }
+
+  // Добавление задачи в local storage
   addTaskToLocalStorrage(task) {
     this.storage.push(task);
     // update local storage
     this.updateLocalStorage();
   }
 
-  // add new task to the list
-  addTask(task) {
-    // template for new task
+  // Добавление новой задачи в список
+  addTaskToDOM(task) {
+    // шаблон новой задачи
     const template = `
       <div class="todo__task" data-task-state="${task.state}" id="${task.id}">
-        <input type="checkbox" class="todo__task-state">
+        <input type="checkbox" class="todo__task-state" ${task.state === 'done' ? 'checked' : ''}>
         <div class="todo__task-text">
           <p class="todo__task-title tl3">${task.taskTitle}</p>
-          <p class="todo__task-desc txt20">${task.taskDesc}</p>
+          <p class="todo__task-desc txt20">${task.taskDesc.replaceAll('\n', '<br>')}</p>
         </div>
         <button class="todo__task-remove tl3">x</button>
       </div>
     `;
-    // adding it to the list
+    // добавление задачи в конец списка
     this.list.innerHTML += template;
   }
 
-  // add tasks functionality
+  // Функционал списка задач
   activateList() {
     this.list.addEventListener('click', (e) => {
       const target = e.target;
 
-      // clicked task state
+      // клик по статусу задачи
       if (target.classList.contains('todo__task-state')) {
         const task = target.closest('.todo__task');
         const id = task.getAttribute('id');
         const newState = target.checked ? 'done' : 'active';
-        // change task state
+        // изменение статуса задачи в input
+        target.checked ? target.setAttribute('checked', '') : target.removeAttribute('checked'); 
+        // изменение статуса задачи todo__task
         task.setAttribute('data-task-state', newState);
-        this.updateTaskState(Number(id), newState);
+        // обновление статуса задачи в storage
+        this.updateTaskStateStorage(Number(id), newState);
       }
 
-      // clicked task remove button
+      // клик по удалению задачи
       if (target.classList.contains('todo__task-remove')) {
         const task = target.closest('.todo__task');
+        // удаление задачи в DOM
         task.remove();
         const id = task.getAttribute('id');
+        // удаление задачи в local storage
         this.removeFromLocalStorage(Number(id));
       }
     });
+    // заполнение списка, срабатывает при открывании страницы
     this.fillList(this.storage);
   }
 
-  updateTaskState(id, newState) {
+  // Обновление состояния задачи в local storage
+  updateTaskStateStorage(id, newState) {
     this.storage.forEach((task) => {
       if (task.id === id) {
         task.state = newState;
       }
     });
-    // update local storage
+    // обновление local storage
     this.updateLocalStorage();
   }
 
-  // remove task from localStorage
+  // Удаление задачи из localStorage
   removeFromLocalStorage(id) {
-    // delete task from storage
+    // удаление задачи из storage
     this.storage = this.storage.filter(task => task.id !== id);
-    // update local storage
+    // обнолвение local storage
     this.updateLocalStorage();
   }
 
-  // restore all task from local storage
-  // work once when the page is loaded/reloaded
+  // Получение данных из local storage
   restoreFromLocalStorage() {
     const storage = localStorage.getItem(LS_NAME);
     return storage === null ? [] : JSON.parse(storage);
   }
 
+  // Заполнение списка задачи
   fillList(list) {
     list.forEach((task) => {
-      this.addTask(task);
+      this.addTaskToDOM(task);
     });
   }
 
+  clearList() {
+    this.list.innerHTML = '';
+  }
+
+  // Обновление local storage
   updateLocalStorage() {
     localStorage.setItem(LS_NAME, JSON.stringify(this.storage));
   }
